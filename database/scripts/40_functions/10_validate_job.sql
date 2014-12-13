9a1401fe542454198e929f25deb99b06515b002b
Feike Steenbergen <feike.steenbergen@zalando.de>
2014-12-13 10:04:22 +0100

    Added more validation, wrote error message which should provide enough details for the user scheduling a job.

--- database/scripts/40_functions/10_validate_job.sql
+++ database/scripts/40_functions/10_validate_job.sql
@@ -17,6 +17,8 @@ $BODY$
 DECLARE
     function_nargs smallint;
     provided_nargs smallint;
+    function_owner name;
+    function_secdef boolean;
 BEGIN
     IF NOT pg_catalog.pg_has_role(current_user, job.user_name, 'MEMBER')
     THEN
@@ -25,12 +27,24 @@ BEGIN
         DETAIL  = format('You are not a member of role "%s"', user_name);
     END IF;
 
-    SELECT pronargs,
-           coalesce( array_length(job.function_arguments, 1), 0)
-      INTO function_nargs,
-           provided_nargs
-      FROM pg_catalog.pg_proc
-    WHERE oid = job.function_signature::regprocedure;
+    BEGIN
+        SELECT pronargs,
+               rolname,
+               prosecdef,
+               coalesce( array_length(job.function_arguments, 1), 0)
+          INTO function_nargs,
+               function_owner,
+               function_secdef,
+               provided_nargs
+          FROM pg_catalog.pg_proc  pp
+          JOIN pg_catalog.pg_roles pr ON (proowner=pr.oid)
+        WHERE pp.oid = job.function_signature::regprocedure;
+    EXCEPTION
+        WHEN undefined_function THEN
+            RAISE SQLSTATE '42883' USING
+            MESSAGE = format('function "%s" does not exist', job.function_signature),
+            HINT    = 'Make sure the search_path is correct, or fully qualify your function signature.';
+    END;
 
     IF function_nargs <> provided_nargs
     THEN
@@ -39,9 +53,26 @@ BEGIN
         DETAIL  = format('Function arguments: %s, arguments provided: %s', function_nargs, provided_nargs);
     END IF;
 
+    IF job.user_name <> function_owner THEN
+        RAISE SQLSTATE '42501' USING
+        MESSAGE = 'Insufficient privileges',
+        DETAIL  = format('Owner of the function does not equal the job user_name. Owner: %s, user_name: %s', function_owner, job.user_name),
+        HINT    = 'Schedule the job using the user_name of the function owner.';
+    END IF;
+
+    IF NOT function_secdef THEN
+        RAISE SQLSTATE '42P13' USING
+        MESSAGE = 'Invalid function definition',
+        DETAIL  = 'Function is not a security definer function.',
+        HINT    = 'Alter the function into a security definer function.';
+    END IF;
+
 END;
 $BODY$
 LANGUAGE plpgsql;
+
+COMMENT ON FUNCTION @extschema@.validate_job_definition(@extschema@.job) IS
+'See comments for @extschema@.validate_job_definition()';
 
 COMMENT ON FUNCTION @extschema@.validate_job_definition() IS
 $$We want to maintain some sanity on the @extschema@.job table. We could do this with check constraints, for example:
